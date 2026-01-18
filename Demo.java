c0c519fe94393791be93f0098c39de27c32fcebf
Sadbhav <kattelsubha@gmail.com>
2026-01-18 10:11:11 -0600

    Second commit

--- Demo.java
+++ Demo.java
@@ -1,15 +1,5 @@
-package practice;
-
-import java.util.*;
-
 public class Demo{
     public static void main(String[] args) {
-        List<Student> nums = new ArrayList<>();
-        Student John = new Student("John", 1);
-        nums.add(John);    
-        Student Jane = new Student("Jane", 2);
-        nums.add(Jane);
-        nums.forEach(num -> System.out.println(num));
-        
+        System.out.println("Hello");
     }
 }
